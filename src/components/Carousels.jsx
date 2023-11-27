c49d41a886fb55cab5482e13fe33d8ca23fd8cbc
MarcusDeveloppement <marcus.frontdev@gmail.com>
2023-11-27 20:05:19 +0100

    setting up the carousel

---
+++ src/components/Carousels.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import jsonData from "../data/Api.json";
+import styles from "./Carousels.module.scss";
+
+const CarouselComponent = ({ id }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [filteredData, setFilteredData] = useState([]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    try {
+      setData(jsonData);
+      setLoading(false);
+    } catch (error) {
+      setError("Erreur lors du chargement des données");
+      setLoading(false);
+      console.error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (id && data.length > 0) {
+      const filteredItem = data.find((item) => item.id === id);
+      setFilteredData(filteredItem ? filteredItem.pictures : []);
+      setCurrentImageIndex(0); // Réinitialiser l'index de l'image actuelle à chaque changement d'ID
+    }
+  }, [id, data]);
+
+  const goToPrevious = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? filteredData.length - 1 : prevIndex - 1
+    );
+  };
+
+  const goToNext = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === filteredData.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const imageCount = filteredData.length;
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div>
+      {imageCount > 0 && (
+        <div className={styles.imgContainer}>
+          <div className={styles.elemContent}>
+            <p>{` ${currentImageIndex + 1}/${imageCount}`}</p>
+            <img
+              src={filteredData[currentImageIndex]}
+              alt={`Photo ${currentImageIndex + 1}`}
+            />
+          </div>
+          <div className={styles.chevrons}>
+            <span onClick={goToPrevious}>
+              <i className="fa-solid fa-chevron-left"></i>
+            </span>
+            <span onClick={goToNext}>
+              <i className="fa-solid fa-chevron-right"></i>
+            </span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CarouselComponent;
--- src/components/Logement.jsx
+++ src/components/Logement.jsx
@@ -1,3 +1,16 @@
+import React from "react";
+import { useParams } from "react-router-dom"; // Si tu utilises React Router
+import jsonData from "../data/Api.json";
+import styles from "./Logement.module.scss";
+import CarouselComponent from "./Carousels";
+
 export default function Logement() {
-  return <div>Logement</div>;
+  const { id } = useParams();
+  const selectedData = jsonData.find((item) => item.id === id);
+
+  return (
+    <>
+      <CarouselComponent id={selectedData.id} />
+    </>
+  );
 }
